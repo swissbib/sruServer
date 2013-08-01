87741a020228630ada9fd85180ff180b46bf59ae
Guenter Hipler <guenter.hipler@unibas.ch>
2013-08-01 12:47:03 +0200

    create a more convenient file header for Java types (including terms of use)

--- src/org/swissbib/sru/targets/common/CQLQueryTransformationInterface.java
+++ src/org/swissbib/sru/targets/common/CQLQueryTransformationInterface.java
@@ -6,12 +6,31 @@ import org.apache.solr.common.SolrDocumentList;
 import org.restlet.data.Form;
 
 /**
- * Created with IntelliJ IDEA.
- * User: swissbib
- * Date: 6/21/13
- * Time: 8:42 AM
- * To change this template use File | Settings | File Templates.
+ * [...description of the type ...]
+ *
+ * Copyright (C) project swissbib, University Library Basel, Switzerland
+ * http://www.swissbib.org  / http://www.swissbib.ch / http://www.ub.unibas.ch
+ * Date: 8/1/13
+ * Time: 11:40 AM
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * license:  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ *
+ * @author Guenter Hipler  <guenter.hipler@unibas.ch>
+ * @link http://www.swissbib.org
+ * @link     https://github.com/swissbib/sruServer
  */
+
+
+
 public interface CQLQueryTransformationInterface {
 
     public void init(Form inputParams, HttpSolrServer sorlServer) throws Exception;
--- src/org/swissbib/sru/targets/common/SRUBasicRepresentation.java
+++ src/org/swissbib/sru/targets/common/SRUBasicRepresentation.java
@@ -3,12 +3,31 @@ package org.swissbib.sru.targets.common;
 import org.restlet.representation.Representation;
 
 /**
- * Created with IntelliJ IDEA.
- * User: swissbib
- * Date: 7/18/13
- * Time: 7:13 PM
- * To change this template use File | Settings | File Templates.
+ * [...description of the type ...]
+ *
+ * Copyright (C) project swissbib, University Library Basel, Switzerland
+ * http://www.swissbib.org  / http://www.swissbib.ch / http://www.ub.unibas.ch
+ * Date: 8/1/13
+ * Time: 11:40 AM
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * license:  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ *
+ * @author Guenter Hipler  <guenter.hipler@unibas.ch>
+ * @link http://www.swissbib.org
+ * @link     https://github.com/swissbib/sruServer
  */
+
+
+
 public abstract class SRUBasicRepresentation {
 
 
