8eb2cdfd2b574349eefd8018ec549764ccc491d7
guenter hipler <guenter.hipler@unibas.ch>
2020-01-20 16:20:21 +0100

    changed default search server

--- src/main/java/org/swissbib/sru/SRUApplication.java
+++ src/main/java/org/swissbib/sru/SRUApplication.java
@@ -88,7 +88,7 @@ public class SRUApplication extends Application {
         String diagnoseDir =  System.getProperty("diagnoseDir","file:///home/swissbib/environment/code/sruWebAppRestLet/build/resources/diagnose");
         //String configuredSOLRServer =  System.getProperty("solrServer","http://search.swissbib.ch/solr/sb-biblio###defaultdb");
         //String configuredSOLRServer =  System.getProperty("solrServer","http://localhost:8081/solr/sb-biblio###defaultdb");
-        String configuredSOLRServer =  System.getProperty("solrServer","http://sb-s20.swissbib.unibas.ch:8080/solr/sb_biblio###defaultdb");
+        String configuredSOLRServer =  System.getProperty("solrServer","http://sgreenc2.swissbib.ch/solr/green###defaultdb");
         String mappingFieldsProps =  System.getProperty("mappingFieldsProps","/home/swissbib/environment/code/sruWebAppRestLet/build/resources/mapping/mapping.solr.properties");
         String mappingCQLRelations =  System.getProperty("mappingCQLRelations","/home/swissbib/environment/code/sruWebAppRestLet/build/resources/mapping/mapping.cqlrelations.properties");
 
